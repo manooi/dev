149b3ae1a0cc66f553fea56f41dbbbf7845927b6
sirawit.mahanin <sirawit.mn@gmail.com>
2025-04-12 22:01:14 +0700

    Add remap for quickfix

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -59,5 +59,13 @@ vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>", { noremap = true, silent = tr
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-q>", ":q<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader><CR>', ':so ~/.config/nvim/init.lua<CR>', { noremap = true })
+
+-- FZF
 vim.keymap.set('n', '<C-p>', ':Files<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-S-p>', ':GFiles<CR>', { noremap = true, silent =true })
+
+-- Quick fix
+vim.keymap.set('n', '<C-j>', ':cnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', ':cprev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-S-o>', ':copen<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-S-c>', ':cclose<CR>', { noremap = true, silent = true })
