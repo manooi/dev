87aee69df72de611b28d81bfb4ae17122c76b7ca
sirawit.mahanin <sirawit.mn@gmail.com>
2025-04-13 23:01:05 +0700

    Add tailwind lsp

--- env/.config/nvim/lua/_lsp.lua
+++ env/.config/nvim/lua/_lsp.lua
@@ -4,10 +4,25 @@ require("mason-lspconfig").setup()
 local lspconfig = require("lspconfig")
 
 lspconfig.ts_ls.setup {
+  ensure_installed = { "tailwindcss" },
   on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
   end,
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
+
+lspconfig.tailwindcss.setup({
+  filetypes = {
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "svelte",
+    "vue",
+  },
+})
 
